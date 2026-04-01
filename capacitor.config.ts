ae12cf863d3e4f1e26dc5e086ce551cbd7f68b69
ChristianChilValencia <valenciamyth@gmail.com>
2026-04-01 13:57:46 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Instagram-Messages-Focus-App',
+  webDir: 'www'
+};
+
+export default config;
